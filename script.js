73d88ebd2e3e312c9adbaca1919b459a316c1524
alysson-melo <143971749+alysson-melo@users.noreply.github.com>
2024-06-04 11:56:52 -0300

    v1.2

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+// CALL Fancybox
 Fancybox.bind('[data-fancybox]', {
 })
 
 Fancybox.bind('[data-fancybox="gallery"]', {
+})
+
+// FECHAR MENU AO CLICAR NO LINK
+let menuItems = document.querySelectorAll('.dropdown-item')
+
+menuItems.forEach(function (menuItems) {
+    menuItems.addEventListener('click', () => {
+
+        let btnMenu = document.querySelector('.navbar-toggler')
+        let optionsMenu = document.querySelector('.navbar-collapse')
+
+        btnMenu.classList.add('collapsed')
+        optionsMenu.classList.remove('show')
+        btnMenu.setAttribute('aria-expanded', 'false')
+
+    })
 })
