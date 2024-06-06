1aa4fd19bdb3d5a0c3692f0341045ae1c6352d94
alysson-melo <143971749+alysson-melo@users.noreply.github.com>
2024-06-06 10:41:01 -0300

    v1.3

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ Fancybox.bind('[data-fancybox="gallery"]', {
 })
 
 // FECHAR MENU AO CLICAR NO LINK
-let menuItems = document.querySelectorAll('.dropdown-item')
+const menuCursos = document.querySelectorAll('.dropdown-item')
+const menuItems = document.querySelectorAll('.js')
 
 menuItems.forEach(function (menuItems) {
     menuItems.addEventListener('click', () => {
 
-        let btnMenu = document.querySelector('.navbar-toggler')
-        let optionsMenu = document.querySelector('.navbar-collapse')
+        const btnMenu = document.querySelector('.navbar-toggler')
+        const optionsMenu = document.querySelector('.navbar-collapse')
 
         btnMenu.classList.add('collapsed')
         optionsMenu.classList.remove('show')
@@ -20,3 +21,51 @@ menuItems.forEach(function (menuItems) {
 
     })
 })
+
+menuCursos.forEach(function (menuCursos) {
+    menuCursos.addEventListener('click', () => {
+
+        const btnMenu = document.querySelector('.navbar-toggler')
+        const optionsMenu = document.querySelector('.navbar-collapse')
+
+        btnMenu.classList.add('collapsed')
+        optionsMenu.classList.remove('show')
+        btnMenu.setAttribute('aria-expanded', 'false')
+
+    })
+})
+
+// VALIDAÇÃO DO CONTATO
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const messageTextarea = document.querySelector("message")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    if (nameInput.value === "") {
+        alert("Por favor, preencha o seu nome")
+        return
+    }
+
+    if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
+        alert("Por favor, preencha o seu email")
+        return
+    }
+
+    form.submit()
+})
+
+function isEmailValid(email) {
+    const emailRegex = new RegExp(
+        /^[a-zA-z0-9._-]+@[a-zA-z0._-]+\.[a-zA-Z]{2,}$/
+    )
+
+    if (emailRegex.test(email)) {
+        return true
+    }
+    return false
+
+}
+
